5b8b368f497ec5cdc841913986f9710bff55818a
CUB3D <callumthom11@gmail.com>
2015-12-07 20:01:11 +0000

    Added solution for day 5 part 2

--- Day-5/Day-5/Day-5.cpp
+++ Day-5/Day-5/Day-5.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 bool hasDissalowed(std::string str)
 {
@@ -48,10 +49,8 @@ int getVowelCount(std::string str)
 	return vowels;
 }
 
-int main()
+void part_1(std::ifstream& data)
 {
-	std::ifstream data("Data.txt");
-
 	std::string input = "";
 
 	int niceCount = 0;
@@ -77,6 +76,116 @@ int main()
 	}
 
 	std::cout << "Number of nice strings: " << niceCount << std::endl;
+}
+
+bool getHasRepeatingWithSpace(std::string str)
+{
+	for (int i = 0; i < str.length() - 2; i++)
+	{
+		char c = str.c_str()[i];
+
+		//std::cout << "Checking " << c << std::endl;
+
+
+		if (c == str.c_str()[i + 2])
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+std::string setChar(std::string str, int pos, char rep)
+{
+	std::string s = "";
+
+	for (int i = 0; i < str.length(); i++)
+	{
+		if (i == pos)
+		{
+			s += rep;
+		}
+		else
+		{
+			s += str.c_str()[i];
+		}
+	}
+
+	return s;
+}
+
+bool getHasDoublePair(std::string str)
+{
+	for (int i = 0; i < str.length(); i++)
+	{
+		std::string pairA = "";
+
+		pairA += (str.c_str()[i]);
+		pairA += (str.c_str()[i + 1]);
+
+		std::string mod = str;
+
+		int numberOfMatchesThisPair = 0;
+
+		for (int ii = 0; ii < mod.length() - 1; ii++)
+		{
+			char c = mod.c_str()[ii];
+			char c1 = mod.c_str()[ii + 1];
+
+			if (c == pairA.c_str()[0] && c1 == pairA.c_str()[1])
+			{
+				numberOfMatchesThisPair++;
+
+				mod = setChar(mod, ii, '*');
+				mod = setChar(mod, ii + 1, '*');
+			}
+		}
+
+		if (numberOfMatchesThisPair >= 2)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+void part_2(std::ifstream& data)
+{
+	std::string input = "";
+
+	int niceCount = 0;
+
+	while (std::getline(data, input))
+	{
+		bool hasRepeatingWithSpace = getHasRepeatingWithSpace(input);
+		bool hasDoublePair = getHasDoublePair(input);
+
+		//std::cout << "Has repeating: " << (hasRepeatingWithSpace ? "true" : "false") << std::endl;
+		//std::cout << "Has pair: " << (hasDoublePair ? "true" : "false") << std::endl;
+
+		bool isNice = hasRepeatingWithSpace && hasDoublePair;
+
+		if (isNice)
+		{
+			niceCount++;
+		}
+
+		std::cout << "Nice: " << (isNice ? "nice" : "naughty") << std::endl;
+	}
+
+	std::cout << "Number of nice strings: " << niceCount << std::endl;
+}
+
+
+int main()
+{
+	std::ifstream data("Data.txt");
+
+	//part_1(data);
+
+	part_2(data);
 
 	int x = 0;
 	std::cin >> x;

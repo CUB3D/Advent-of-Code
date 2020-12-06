405f35518a1fde0fe4da068ba3c5ade78f3017de
CUB3D <callumthom11@gmail.com>
2020-12-06 15:46:30 +0000

    Day6

---
+++ 2020/src/day6.rs
@@ -0,0 +1,43 @@
+use std::collections::HashSet;
+
+#[aoc_generator(day6)]
+pub fn gen(input: &str) -> Vec<Vec<HashSet<char>>> {
+    input
+        .split("\n\n")
+        .map(|l| {
+            l.lines()
+                .map(|person_answers| person_answers.chars().collect::<HashSet<char>>())
+                .collect()
+        })
+        .collect::<Vec<_>>()
+}
+
+#[aoc(day6, part1)]
+pub fn part1(groups: &[Vec<HashSet<char>>]) -> u64 {
+    groups
+        .iter()
+        .map(|group| {
+            group
+                .iter()
+                .cloned()
+                .flat_map(|member| member)
+                .collect::<HashSet<_>>()
+                .len()
+        })
+        .fold(0, |acc, x| acc + x as u64)
+}
+
+#[aoc(day6, part2)]
+pub fn part2(groups: &[Vec<HashSet<char>>]) -> u64 {
+    groups
+        .iter()
+        .map(|group| {
+            let first_answers = &group[0];
+            let common_answers = first_answers
+                .iter()
+                .filter(|answer| group.iter().all(|member| member.contains(answer)))
+                .collect::<HashSet<_>>();
+            common_answers.len()
+        })
+        .fold(0, |acc, x| acc + x as u64)
+}
